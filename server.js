f299c9d3b107808744423f6f61d8a542cfd19677
Rachit8090 <rachitpandey80@gmail.com>
2022-07-10 16:05:03 +0530

    Changes in Database

--- server.js
+++ server.js
@@ -2,46 +2,73 @@ const express = require('express')
 const app = express()
 const port = 8000
 const path = require('path')
-const bodyparser=require("body-parser");
-const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost/helpspotify',{useNewUrlParser:true});  //mongoconnectionestavblist
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
+ 
+//serve static files using express
 app.use(express.static('public'))
 app.use(express.static('views'))
 app.set('view engine', 'pug');
 
-var helpSchema=new mongoose.Schema({   //create schema of our database
-  name:String,
-  email:String,
-  helptxt:String
+
+//mongodb connection establishment
+mongoose.connect('mongodb://localhost:27017/helpSpot', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+}).then(() => {
+  console.log("Database Connection SuccessFull");
+}).catch((e) => {
+  console.log("connection failed");
+})
+
+app.use(express.urlencoded({ extended: true }));
+
+const helpSchema = new mongoose.Schema({   //create schema of our database
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  desc: {
+    type: String,
+    required: true
+  }
 });
 
-var helps=mongoose.model('help',helpSchema);  //implement final model of schema
+const Help = mongoose.model('Help', helpSchema);  //implement final model of schema
 
-//app.engine('html', require('ejs').renderFile);
+//serve all files using express
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get('/index',(req,res)=>{
+app.get('/index', (req, res) => {
   res.render('index')
 })
 app.get('/about', (req, res) => {
   res.status(200).render('about.pug');
 })
 
-app.get('/help',(req,res)=>{
+app.get('/help', (req, res) => {
   res.status(200).render('help.pug');
 })
 
-app.post('/help',(req,res)=>{
-  var mydata=new helps(req.body);
-  mydata.save().then(()=>{
-    res.send("this item is saved to database");
-  }).catch(()=>{
+//after submit press post request and get data from database
+app.post('/help', (req, res) => {
+
+  var mydata = new Help(req.body);
+  mydata.save().then(() => {
+    res.status(200).render('index.pug');
+  }).catch(() => {
     res.status(400).send("item was not saved");
   })
 })
 
+//server starts
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ... ${port}`)
 })
